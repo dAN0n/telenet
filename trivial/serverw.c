50e35878c82e6740cabecc973258bc5944479af5
d@N0n <dan0na@ya.ru>
2016-09-11 23:27:01 +0300

    Server changes

--- trivial/serverw.c
+++ trivial/serverw.c
@@ -1,19 +1,28 @@
-#include <sys/types.h>
 #include <winsock2.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void){
-	
+	WSADATA wsa;
+	int wsaResult = WSAStartup(MAKEWORD(2, 2), &wsa);
+	printf("WSAS start\n");
+	if(wsaResult != 0){
+		printf("WSAStartup error: %d\n", wsaResult);
+		exit(1);
+	}
+	else
+		puts("WSAStartup success");
+
 	struct sockaddr_in local;
 	int s;
 	int s1;
 	int rc;
 	char buf[1];
+	// char buf[20];
 	
 	local.sin_family = AF_INET;
-	local.sin_port = htons (8080);
+	local.sin_port = htons(8080);
 	local.sin_addr.s_addr = htonl(INADDR_ANY);
 	
 	s = socket(AF_INET, SOCK_STREAM, 0);
@@ -45,6 +54,7 @@ int main(void){
 	}
 	
 	rc = recv(s, buf, 1, 0);
+	// rc = recv(s, buf, strlen(buf), 0);
 	
 	if(rc <= 0){
 		perror("Recv error");
@@ -52,11 +62,12 @@ int main(void){
 	}
 	
 	printf("%c\n", buf[0]);
+	// printf("%s\n", buf);
 	
 	rc = send(s1, "2", 1, 0);
 	
 	if(rc <= 0)
 		perror("Send error");
-	
+
 	exit(0);
 }
