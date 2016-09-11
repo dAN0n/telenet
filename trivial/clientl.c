1cac9d1fa2111550264c0dd1c032b20b80cbc545
d@N0n <dan0na@ya.ru>
2016-09-11 23:31:06 +0300

    Client changes

--- trivial/clientl.c
+++ trivial/clientl.c
@@ -3,6 +3,12 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <string.h>
+
+char MESSAGE[20] = "Message from client";
+/* char buf[20]; */
+char SERVER_IP[15] = "192.168.222.1";
+int SERVER_PORT = 8080;
 
 int main(void){
 	
@@ -12,8 +18,8 @@ int main(void){
 	char buf[1];
 	
 	peer.sin_family = AF_INET;
-	peer.sin_port = htons (8080);
-	peer.sin_addr.s_addr = inet_addr("192.168.56.1");
+	peer.sin_addr.s_addr = inet_addr(SERVER_IP);
+	peer.sin_port = htons(SERVER_PORT);
 	
 	s = socket(AF_INET, SOCK_STREAM, 0);
 	
@@ -29,18 +35,21 @@ int main(void){
 		exit(1);
 	}
 	
-	rc = send(s, "1", 1, 0);
+	/* rc = send(s, MESSAGE, strlen(MESSAGE), 0); */
+	rc = send(s, "5", 1, 0);
 	
 	if(rc <= 0){
 		perror("Send error");
 		exit(1);
 	}
 	
+	/* rc = recv(s, buf, strlen(buf), 0); */
 	rc = recv(s, buf, 1, 0);
 	
 	if(rc <= 0)
 		perror("Recv error");
 	else
+		/* printf("%s\n", buf); */
 		printf("%c\n", buf[0]);
 	
 	exit(0);
