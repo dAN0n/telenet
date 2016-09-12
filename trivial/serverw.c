5b074e77073df674ff1c03a03255622583ca6b9b
d@N0n <dan0na@ya.ru>
2016-09-12 11:16:38 +0300

    Server minor fix

--- trivial/serverw.c
+++ trivial/serverw.c
@@ -10,13 +10,13 @@ int SERVER_PORT = 8080;
 int main(void){
 	WSADATA wsa;
 	int wsaResult = WSAStartup(MAKEWORD(2, 2), &wsa);
-	printf("WSAS start\n");
+	printf("_WSAS start\n");
 	if(wsaResult != 0){
 		printf("WSAStartup error: %d\n", wsaResult);
 		exit(1);
 	}
 	else
-		puts("WSAStartup success");
+		puts("_WSAStartup success");
 
 	struct sockaddr_in local;
 	int s;
@@ -32,42 +32,41 @@ int main(void){
 	if(s < 0){
 		perror("Socket error");
 		exit(1);
-	}
+	}else puts("_Socket created");
 	
 	rc = bind(s, (struct sockaddr *) &local, sizeof(local));
 	
 	if(rc < 0){
 		perror("Bind error");
 		exit(1);
-	}
+	}else puts("_Bind success");
 	
 	rc = listen(s, 5);
 	
 	if(rc){
 		perror("Listen error");
 		exit(1);
-	}
+	}else puts("_Listen");
 	
 	s1 = accept(s, NULL, NULL);
 	
 	if(s1 < 0){
 		perror("Accept error");
 		exit(1);
-	}
+	}else puts("_Accept");
 	
 	rc = recv(s1, buf, sizeof(buf), 0);
 	
 	if(rc <= 0){
 		perror("Recv error");
 		exit(1);
-	}
-	
-	printf("%s\n", buf);
+	}else printf("%s\n", buf);
 	
 	rc = send(s1, MESSAGE, sizeof(MESSAGE), 0);
 	
 	if(rc <= 0)
 		perror("Send error");
+	else puts("_Send success");
 
 	exit(0);
 }
